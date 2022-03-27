a5659bb35d0572b365c7776ddd2e5f91ae7d44e5
chocolateric <nhpss930042@gmail.com>
2022-03-27 16:38:38 +0800

    test(cmd): add partial testing code in client and server main.go

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	context "context"
+	flag "flag"
+	fmt "fmt"
+	insecure "google.golang.org/grpc/credentials/insecure"
+	grpc "google.golang.org/grpc"
+	pb "github.com/liyunghao/Online-Eletronic-Voting/internal/voting"
+)
+
+const (
+	defaultName = "world"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+)
 
 func main() {
+	var election *pb.Election
+	var ctx context.Context
+	var conn *grpc.ClientConn
+	var name *pb.VoterName
+	var authrequest *pb.AuthRequest
+	var vote *pb.Vote
+	var election_name *pb.ElectionName
+
+
+	flag.Parse()
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	fmt.Println(err)
+
+	client := pb.NewEVotingClient(conn)
+	var e error
+	var chal *pb.Challenge
+	var token *pb.AuthToken
+	var s1 *pb.Status
+	var s2 *pb.Status
+	var result *pb.ElectionResult
+	// five function for eVoting
+	chal, e = client.PreAuth(ctx, name)
+	fmt.Println(chal, e)
+	token, e  = client.Auth(ctx, authrequest)
+	fmt.Println(token, e)
+	s1, e  = client.CreateElection(ctx, election)
+	fmt.Println(s1, e)
+	s2, e = client.CastVote(ctx, vote)
+	fmt.Println(s2, e)
+	result, e = client.GetResult(ctx, election_name)
+	fmt.Println(result, e)
 	fmt.Println("Hello Client")
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	flag "flag"
+	net "net"
+	fmt "fmt"
+	log "log"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
 
 func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", lis.Addr())
+	}
 	fmt.Println("Hello Server")
 }
