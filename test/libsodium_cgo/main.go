c23f74d41ce3105a6169fe9630233d2629df3e47
Elven Lin, RetrO9 <elvenlin1998@gmail.com>
2022-04-13 13:22:54 +0800

    feat: change libsodium_cgo test implementation to use ed25519

--- test/libsodium_cgo/main.go
+++ test/libsodium_cgo/main.go
@@ -19,46 +19,46 @@ func main() {
 		return
 	}
 
-	// Generate Sodium Sign Key
-	var signKey [C.crypto_sign_SECRETKEYBYTES]byte
-	var signPublicKey [C.crypto_sign_PUBLICKEYBYTES]byte
-	if C.crypto_sign_keypair(
-		(*C.uchar)(&signPublicKey[0]),
-		(*C.uchar)(&signKey[0]),
+	// Generate Sodium ed25519 Sign Key
+	var sign_key [C.crypto_sign_ed25519_SECRETKEYBYTES]byte
+	var sign_pk [C.crypto_sign_ed25519_PUBLICKEYBYTES]byte
+	if C.crypto_sign_ed25519_keypair(
+		(*C.uchar)(&sign_pk[0]),
+		(*C.uchar)(&sign_key[0]),
 	) == -1 {
-		fmt.Println("crypto_sign_keypair() failed")
+		fmt.Println("crypto_sign_ed25519_keypair() failed")
 		return
 	}
 
 	// Output key in base64
 	fmt.Println("Sign Key:")
-	fmt.Println(signKey)
-	fmt.Println(base64.StdEncoding.EncodeToString(signKey[:]))
+	fmt.Println(sign_key)
+	fmt.Println(base64.StdEncoding.EncodeToString(sign_key[:]))
 	fmt.Println("Sign Public Key:")
-	fmt.Println(signPublicKey)
-	fmt.Println(base64.StdEncoding.EncodeToString(signPublicKey[:]))
+	fmt.Println(sign_pk)
+	fmt.Println(base64.StdEncoding.EncodeToString(sign_pk[:]))
 
 	mes := []byte("Hello LIBSodium")
 
-	// Generate Sodium Sign Message in Detached Mode
-	var signature [C.crypto_sign_BYTES]byte
+	// Generate Sodium ed25519 Sign Message in Detached Mode
+	var sign_mes [C.crypto_sign_ed25519_BYTES]byte
 	if C.crypto_sign_detached(
-		(*C.uchar)(&signature[0]),
+		(*C.uchar)(&sign_mes[0]),
 		nil,
 		(*C.uchar)(&mes[0]),
 		C.ulonglong(len(mes)),
-		(*C.uchar)(&signKey[0]),
+		(*C.uchar)(&sign_key[0]),
 	) == -1 {
 		fmt.Println("crypto_sign_detached() failed")
 		return
 	}
 
-	// Verify Signature
+	// verify Signature
 	if C.crypto_sign_verify_detached(
-		(*C.uchar)(&signature[0]),
+		(*C.uchar)(&sign_mes[0]),
 		(*C.uchar)(&mes[0]),
 		C.ulonglong(len(mes)),
-		(*C.uchar)(&signPublicKey[0]),
+		(*C.uchar)(&sign_pk[0]),
 	) == 0 {
 		fmt.Println("Signature Verified")
 	} else {
