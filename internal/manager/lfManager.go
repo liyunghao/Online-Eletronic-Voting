72ed88bb320d48f7dae7141b0a9a1aaa7f1ebe3f
Pin-Ching Chen <p.ching0811@gmail.com>
2022-05-01 21:37:57 +0800

    feat: Implement CatchUpHandler

--- internal/manager/lfManager.go
+++ internal/manager/lfManager.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
 	st "github.com/liyunghao/Online-Eletronic-Voting/internal/storage"
 )
@@ -20,11 +21,30 @@ type Cluster struct {
 	Id   int    `json:"id"`
 }
 
+type Payload struct {
+	Name         string    `json:"name"`
+	Group        string    `json:"group"`
+	PublicKey    string    `json:"public_key"`
+	Groups       []string  `json:"groups"`
+	Choices      []string  `json:"choices"`
+	EndDate      time.Time `json:"end_date"`
+	ElectionName string    `json:"election_name"`
+	VoterName    string    `json:"voter_name"`
+	Choice       string    `json:"choice"`
+}
+
+type Log struct {
+	Cmd     string  `json:"storage_cmd"`
+	Payload Payload `json:"payload"`
+}
+
+var logs []Log
+
 // vote & result
 type vote struct {
-	electionName string `json:"election_name"`
-	voterName    string `json:"voter_name"`
-	choice       string `json:"choice"`
+	ElectionName string `json:"election_name"`
+	VoterName    string `json:"voter_name"`
+	Choice       string `json:"choice"`
 }
 
 type LfManager struct {
@@ -35,7 +55,7 @@ var LFManger LfManager
 func (lf *LfManager) Initialize(args ...interface{}) error {
 	http.HandleFunc("/hearbeat", lf.HeartBeatHandler)
 	http.HandleFunc("/writesync", lf.WriteSyncHandler)
-	http.HandleFunc("/declare_leader", lf.DeclareLeaderHandler)
+	http.HandleFunc("/declare_capability", lf.DeclareLeaderHandler)
 	http.HandleFunc("/catch_up", lf.CatchUpHandler)
 	http.HandleFunc("/recv_elect", lf.RecvElectHandler)
 	log.Fatal(http.ListenAndServe(":9000", nil))
@@ -58,41 +78,47 @@ func (lf *LfManager) WriteSyncHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch cmd {
 	case "CreateUser":
-		var payload st.User
-		json.Unmarshal(payloadData, payload)
-		st.DataStorage.CreateUser(payload.Name, payload.Group, payload.PublicKey)
+		var user st.User
+		json.Unmarshal(payloadData, &user)
+		st.DataStorage.CreateUser(user.Name, user.Group, user.PublicKey)
 	// case "FetchUser":
 	//     var payload st.User
 	//     json.Unmarshal(payloadData, payload)
 	//     st.DataStorage.FetchUser(payload.Name)
 	case "RemoveUser":
-		var payload st.User
-		json.Unmarshal(payloadData, payload)
-		st.DataStorage.RemoveUser(payload.Name)
+		var user st.User
+		json.Unmarshal(payloadData, &user)
+		st.DataStorage.RemoveUser(user.Name)
 
 	case "CreateElection":
-		var payload st.Election
-		json.Unmarshal(payloadData, payload)
-		st.DataStorage.CreateElection(payload.Name, payload.Groups, payload.Choices, payload.EndDate)
+		var election st.Election
+		json.Unmarshal(payloadData, &election)
+		st.DataStorage.CreateElection(election.Name, election.Groups, election.Choices, election.EndDate)
 	// case "FetchElection":
 	//     var payload st.Election
 	//     json.Unmarshal(payloadData, payload)
 	//     st.DataStorage.FetchElection(payload.Name)
 
 	case "VoteElection":
-		var payload vote
-		json.Unmarshal(payloadData, payload)
-		st.DataStorage.VoteElection(payload.electionName, payload.voterName, payload.electionName)
+		var election vote
+		json.Unmarshal(payloadData, &election)
+		st.DataStorage.VoteElection(election.ElectionName, election.VoterName, election.Choice)
 
 	}
+	w.WriteHeader(http.StatusOK)
 }
 
 func (lf *LfManager) DeclareLeaderHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (lf *LfManager) CatchUpHandler(w http.ResponseWriter, r *http.Request) {
-	body, _ := ioutil.ReadAll(r.Body)
-
+	// body, _ := ioutil.ReadAll(r.Body)
+	w.Header().Set("Content-Type", "application/json")
+	logsJson := make(map[string][]Log)
+	logsJson["logs"] = logs
+	data, _ := json.Marshal(logsJson)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(data)
 }
 
 func (lf *LfManager) RecvElectHandler(w http.ResponseWriter, r *http.Request) {
