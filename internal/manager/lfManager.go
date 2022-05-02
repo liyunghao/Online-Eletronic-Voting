de3b3ff65b88c1228393fb7b415cb0a033ee5c46
Pin-Ching Chen <p.ching0811@gmail.com>
2022-05-02 16:25:35 +0800

    feat: update handler to call ReplicaLogWrapper function

--- internal/manager/lfManager.go
+++ internal/manager/lfManager.go
@@ -15,32 +15,6 @@ import (
 	st "github.com/liyunghao/Online-Eletronic-Voting/internal/storage"
 )
 
-type Payload struct {
-	Name         string    `json:"name"`
-	Group        string    `json:"group"`
-	PublicKey    string    `json:"public_key"`
-	Groups       []string  `json:"groups"`
-	Choices      []string  `json:"choices"`
-	EndDate      time.Time `json:"end_date"`
-	ElectionName string    `json:"election_name"`
-	VoterName    string    `json:"voter_name"`
-	Choice       string    `json:"choice"`
-}
-
-type Log struct {
-	Cmd     string  `json:"storage_cmd"`
-	Payload Payload `json:"payload"`
-}
-
-var logs []Log
-
-// vote & result
-type vote struct {
-	ElectionName string `json:"election_name"`
-	VoterName    string `json:"voter_name"`
-	Choice       string `json:"choice"`
-}
-
 type replicas struct {
 	node    Node
 	cluster []Cluster
@@ -107,71 +81,36 @@ func (lf *LfManager) WriteSyncHandler(w http.ResponseWriter, r *http.Request) {
 	cmdData, _ := json.Marshal(data["storage_cmd"])
 	payloadData, _ := json.Marshal(data["payload"])
 
-	var cmd string
+	var cmd int
 	json.Unmarshal(cmdData, &cmd)
+	var payload string
+	json.Unmarshal(payloadData, &payload)
 
-	var logPayload Payload
-
-	switch cmd {
-	case "CreateUser":
-		var user st.User
-		json.Unmarshal(payloadData, &user)
-		st.DataStorage.CreateUser(user.Name, user.Group, user.PublicKey)
-		logPayload.Name = user.Name
-		logPayload.Group = user.Group
-		logPayload.PublicKey = user.Group
-	// case "FetchUser":
-	//     var payload st.User
-	//     json.Unmarshal(payloadData, payload)
-	//     st.DataStorage.FetchUser(payload.Name)
-	case "RemoveUser":
-		var user st.User
-		json.Unmarshal(payloadData, &user)
-		st.DataStorage.RemoveUser(user.Name)
-		logPayload.Name = user.Name
-
-	case "CreateElection":
-		var election st.Election
-		json.Unmarshal(payloadData, &election)
-		st.DataStorage.CreateElection(election.Name, election.Groups, election.Choices, election.EndDate)
-		logPayload.Name = election.Name
-		logPayload.Groups = election.Groups
-		logPayload.Choices = election.Choices
-		logPayload.EndDate = election.EndDate
-
-	// case "FetchElection":
-	//     var payload st.Election
-	//     json.Unmarshal(payloadData, payload)
-	//     st.DataStorage.FetchElection(payload.Name)
-
-	case "VoteElection":
-		var election vote
-		json.Unmarshal(payloadData, &election)
-		st.DataStorage.VoteElection(election.ElectionName, election.VoterName, election.Choice)
-		logPayload.ElectionName = election.ElectionName
-		logPayload.VoterName = election.VoterName
-		logPayload.Choice = election.Choice
+	if err := st.DataStorage.(*st.ReplicaLogWrapper).SynctoStorage(cmd, payload, true); err == nil {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		// error status
+		w.WriteHeader(http.StatusInternalServerError)
 	}
-	w.WriteHeader(http.StatusOK)
-	logs = append(logs, Log{cmd, logPayload})
-}
-
-func (lf *LfManager) DeclareLeaderHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (lf *LfManager) CatchUpHandler(w http.ResponseWriter, r *http.Request) {
-	// body, _ := ioutil.ReadAll(r.Body)
-	w.Header().Set("Content-Type", "application/json")
-	logsJson := make(map[string][]Log)
-	logsJson["logs"] = logs
-	data, _ := json.Marshal(logsJson)
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(data)
+	body, _ := ioutil.ReadAll(r.Body)
+	data := make(map[string]int)
+	json.Unmarshal(body, &data)
+	logIdData, _ := json.Marshal(data["log_id"])
+	var logId int
+	json.Unmarshal(logIdData, &logId)
+	if logs, err := st.DataStorage.(*st.ReplicaLogWrapper).CatchUp(logId); err == nil {
+		w.Header().Set("Content-Type", "application/json")
+		data, _ := json.Marshal(logs)
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(data)
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
 
-func (lf *LfManager) RecvElectHandler(w http.ResponseWriter, r *http.Request) {
-
-}
 func (lf *LfManager) Start() error {
 	router := mux.NewRouter().StrictSlash(true)
 
@@ -202,5 +141,4 @@ func (lf *LfManager) ParseConfig(filename string) (Node, []Cluster) {
 	json.Unmarshal(bytes, &tmp)
 
 	return tmp.node, tmp.cluster
-
 }
